06d4e73012d44123ff81f768048ff6d478a77b1f
glomaga@gmail.com <glomaga@gmail.com>
2017-09-26 00:44:00 -0500

    init

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ var router = express.Router();
 //===================4.MIDDLEWARE=========================//
 //======================================================
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
